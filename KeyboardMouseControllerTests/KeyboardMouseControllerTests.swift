9356a7e5a25618127e612dda9a54d51511f49d24
chaewookim <chaewookim2877@gmail.com>
2025-10-17 10:13:48 +0900

    Initial commit for new repository structure

---
+++ KeyboardMouseControllerTests/KeyboardMouseControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  KeyboardMouseControllerTests.swift
+//  KeyboardMouseControllerTests
+//
+//  Created by 김채우 on 10/16/25.
+//
+
+import Testing
+
+struct KeyboardMouseControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KeyboardMouseController
+키보드로 마우스를 제어하는 애플리케이션
